8e4ef25b15bc93372677c5917eb9c0254c50372d
Leland321 <lelandwrites@outlook.com>
2024-07-27 17:29:57 -0400

    Initialize global var, init function names

---
+++ javascript.js
@@ -0,0 +1,50 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+
+function getComputerChoice();
+
+
+function getHumanChoice();
+
+
+function playRound();
+
+
+
+
+
+
+
+/*
+rock paper scissors played in the console
+ask human player for input of rock OR paper OR scissors
+print human input to console
+generate random choice for computer of rock OR paper OR scissors
+only accept valid input, ask again if input is invalid (remove case sensitivity)
+print pc "choice" to console
+compare human choice to pc choice
+determine which one wins, or tie
+print you win or lose or tie message to console
+if winner increment score
+
+run game again until
+first to score 3 "you win the game"
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+//console.log();//
