ae46312955aca840c9051ff790e3925c05f95a7c
Leland321 <lelandwrites@outlook.com>
2024-07-30 16:42:46 -0400

    Refactor code Some global variables are now found inside playGame, functions have been reworked to reflect this

--- javascript.js
+++ javascript.js
@@ -1,8 +1,4 @@
 
-let humanScore = 0;
-let computerScore = 0;
-let round = 0;
-
 let humanChoice;
 let computerChoice;
 
@@ -40,37 +36,48 @@ function getHumanChoice() {
 
 
 function playRound(computerChoice, humanChoice) {
-    if (computerChoice === 0 && humanChoice === 'paper') {
+    if (computerChoice === 0 && humanChoice === 'paper') {  //These can probably be combined into three, win, lose, tie
         console.log('You win this round!');
-        humanScore++;
+        return 'human';
     } else if (computerChoice === 0 && humanChoice === 'scissors') {
         console.log('You lose this round!');
-        computerScore++;
+        return 'computer';
     } else if (computerChoice === 1 && humanChoice === 'scissors') {
         console.log('You win this round!');
-        humanScore++;
+        return 'human';
     } else if (computerChoice === 1 && humanChoice === 'rock') {
         console.log('You lose this round!');
-        computerScore++;
+        return 'computer';
     } else if (computerChoice === 2 && humanChoice === 'rock') {
         console.log('You win this round!');
-        humanScore++;
+        return 'human';
     } else if (computerChoice === 2 && humanChoice === 'paper') {
         console.log('You lose this round!');
-        computerScore++;
+        return 'computer';
     } else {
         console.log('Tie!');
     } 
-    round++;
 }
 
 
 function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let round = 0;
         do {
             let humanSelection = getHumanChoice();
             let computerSelection = getComputerChoice();
-            playRound(computerSelection, humanSelection);
+            let roundResult = playRound(computerSelection, humanSelection);
+            round++; 
+            
+            if (roundResult === 'human') {
+                humanScore++;
+            } else if (roundResult === 'computer') {
+                computerScore++;
+            }
         } while (round < 5);
+
+       
 
         if (humanScore > computerScore) {
             console.log('You won the game!');
@@ -79,6 +86,7 @@ function playGame() {
         } else {
             console.log("It's a tie!");
         }
+    
 }
 
 playGame();
