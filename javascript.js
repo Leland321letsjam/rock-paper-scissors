f1a5c3aa0de158ab61ea037087e5509437f04cb5
Leland321 <lelandwrites@outlook.com>
2024-09-05 20:17:26 -0400

    Create functions to display scores play round now tracks scores and references display functions

--- javascript.js
+++ javascript.js
@@ -1,147 +1,110 @@
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
 
-let humanChoice;
-let computerChoice;
 
 
 
 function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 3)
+    computerChoice = Math.floor(Math.random() * 3);
     displayComputerChoice(computerChoice);
-    return computerChoice;
 };
 
 function displayComputerChoice () {
     const computerSelection = document.querySelector('.computer-selection');
-    const computerSelectionResult = document.createElement('span');
-    computerSelectionResult.className = 'display-computer-selection'
+    computerSelection.textContent = '';
     switch (computerChoice) {
         case 0:
-            computerSelectionResult.textContent = 'Rock';
+            computerSelection.textContent = 'Computer selection: Rock';
             break;
         case 1:
-            computerSelectionResult.textContent = 'Paper';
+            computerSelection.textContent = 'Computer selection: Paper';
             break;
         case 2:
-            computerSelectionResult.textContent = 'Scissors';
+            computerSelection.textContent = 'Computer selection: Scissors';
             break;
     }
-    computerSelection.appendChild(computerSelectionResult);
 };
 
 function displayPlayerChoice(humanChoice) {
     const yourSelection = document.querySelector('.your-selection');
-    yourSelection.textContent = `Your selection: ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`;
+    yourSelection.textContent = `Your selection: ${humanChoice}`;
 };
 
 
-function playRound(computerChoice, humanChoice) {
-    if (computerChoice === 0 && humanChoice === 'paper' 
-        || computerChoice === 1 && humanChoice === 'scissors'
-        || computerChoice === 2 && humanChoice === 'rock'
-        ) {
-            const divContainerOne = document.querySelector('.container1'); //Should create a new function for this but I'm being lazy
-            const displayRoundResult = document.createElement('h3');
-            displayRoundResult.textContent = 'You win this round!';
-            divContainerOne.appendChild(displayRoundResult);
-            return 'human';
-    } else if (computerChoice === 0 && humanChoice === 'scissors'
-        || computerChoice === 1 && humanChoice === 'rock'
-        || computerChoice === 2 && humanChoice === 'paper'
-        ) {
-            const divContainerOne = document.querySelector('.container1'); 
-            const displayRoundResult = document.createElement('h3');
-            displayRoundResult.textContent = 'You lose this round!';
-            divContainerOne.appendChild(displayRoundResult);
-            return 'computer';
-    } else {
-            const divContainerOne = document.querySelector('.container1'); 
-            const displayRoundResult = document.createElement('h3');
-            displayRoundResult.textContent = 'Tie!';
-            divContainerOne.appendChild(displayRoundResult);
-    } 
+function playRound(humanChoice) {
+    const divContainerOne = document.querySelector('h3');
+    divContainerOne.textContent = '';
+    round++;
+    getComputerChoice();
+    let roundResult;
+    if (computerChoice === 0 && humanChoice === 'Paper' 
+        || computerChoice === 1 && humanChoice === 'Scissors'
+        || computerChoice === 2 && humanChoice === 'Rock'
+        ) {  
+            divContainerOne.textContent = 'You win this round!';
+            roundResult = 'human';
+    } else if (computerChoice === 0 && humanChoice === 'Scissors'
+        || computerChoice === 1 && humanChoice === 'Rock'
+        || computerChoice === 2 && humanChoice === 'Paper'
+        ) {           
+            divContainerOne.textContent = 'You lose this round!';
+            roundResult = 'computer';
+    } else {              
+        divContainerOne.textContent = 'Tie!';
+        roundResult = 'tie';
+    }
+
+    if (roundResult === 'human') {
+        humanScore++;
+    } else if (roundResult === 'computer') {
+        computerScore++;
+    }
+    roundAndScoreDisplay();
+
+    if (humanScore === 5) {
+        divContainerOne.textContent = 'Congratulations! You win the game!';
+        resetGame();
+    } else if (computerScore === 5) {
+        divContainerOne.textContent = 'Sorry, the computer wins the game.';
+        resetGame();
+    }
 };
 
 
-// function playGame() {
-//     let humanScore = 0;
-//     let computerScore = 0;
-//     let round = 0;
-//         do {
-//             let humanSelection = getHumanChoice();
-//             let computerSelection = getComputerChoice();
-//             let roundResult = playRound(computerSelection, humanSelection);
-//             round++; 
-            
-//             if (roundResult === 'human') {
-//                 humanScore++;
-//             } else if (roundResult === 'computer') {
-//                 computerScore++;
-//             }
-//         } while (round < 5);
 
-       
+function roundAndScoreDisplay() {
+    const scoreDisplay = document.querySelector('.score');
+    scoreDisplay.textContent = `Your Score: ${humanScore} | Computer Score: ${computerScore}`;
+    const roundDisplay = document.querySelector('.round');
+    roundDisplay.textContent = `Round: ${round}`;
+};
 
-//         if (humanScore > computerScore) {
-//             console.log('You won the game!');
-//         } else if (humanScore < computerScore) {
-//             console.log('You lost the game!'); 
-//         } else {
-//             console.log("It's a tie!");
-//         }
-    
-// }
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    round = 0;
+};
+
 
 const buttonRock = document.querySelector('#rock');
 const buttonPaper = document.querySelector('#paper');
 const buttonScissors = document.querySelector('#scissors');
 
 buttonRock.addEventListener('click', () => {
-    let humanChoice = 'rock';
-    let x = document.querySelector('.display-computer-selection');
-    if (x) {
-        x.remove();
-    }
-    let y = document.querySelector('h3');
-    if (y) {
-        y.remove();
-    }
-    getComputerChoice();
+    let humanChoice = 'Rock';
     displayPlayerChoice(humanChoice);
-    playRound(computerChoice, humanChoice);
+    playRound(humanChoice);
 });
 
 buttonPaper.addEventListener('click', () => {
-    let humanChoice = 'paper';
-    let x = document.querySelector('.display-computer-selection');
-    if (x) {
-        x.remove();
-    }
-    let y = document.querySelector('h3');
-    if (y) {
-        y.remove();
-    }
-    getComputerChoice();
+    let humanChoice = 'Paper';
     displayPlayerChoice(humanChoice);
-    playRound(computerChoice, humanChoice);
+    playRound(humanChoice);
 });
 
 buttonScissors.addEventListener('click', () => {
-    let humanChoice = 'scissors';
-    let x = document.querySelector('.display-computer-selection');
-    if (x) {
-        x.remove();
-    }
-    let y = document.querySelector('h3');
-    if (y) {
-        y.remove();
-    }
-    getComputerChoice();
+    let humanChoice = 'Scissors';
     displayPlayerChoice(humanChoice);
-    playRound(computerChoice, humanChoice);
+    playRound(humanChoice);
 });
-
-
-
-
-
-//playGame();
